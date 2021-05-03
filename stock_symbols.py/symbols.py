e2cf6eefe72300f30b4af2c0999ff2f410aad6c2
viviach <viviana.aragonch@gmail.com>
2021-05-03 09:19:27 -0500

    symbols to test

---
+++ stock_symbols.py/symbols.py
@@ -0,0 +1,7 @@
+sector_symbol=["XLC","XLP","XLY","XLE","XLF","XLV", "XLI", "XLB", "XLRE", "XLK","XLU"]
+
+industry_symbol=["AD","AFL","A","AC","AM","BKI","BEV","BT","BP","CMI","C","CSS","CEQ","CE","CM","CM","CF","CP","D","DCS","DFS","DTS","EU","EE","EEI","EES","ENT","ERE","F","FSR","FP","HES","HPS","HT","HRL","HD","IPR","ICG","IMS","IDM","ITS","LPI","LST","MAC","MED","MM","MR","MU","OGC","PP","PH","PS","REM","RR","SSE","SW","SR","THS","TAL","T","TCD","WU"]
+
+company_symbol=
+
+
