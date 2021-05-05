3507883215132538a26f1d25c460e56f923f8c16
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-05-05 08:42:24 -0500

    Reverts energy_industry ticker list

--- tickers.py
+++ tickers.py
@@ -2,7 +2,7 @@
 sector_symbol=["XLC","XLP","XLY","XLE","XLF","XLV", "XLI", "XLB", "XLRE", "XLK","XLU"]
 
 # Tickers to analze by industry inside relevant economic sector
-energy_industry = ['PEES', 'POGC']
+energy_industry = ['EES', 'OGC']
 technology_industry = ['CEQ', 'EEI', 'ITS', 'SSE', 'SW', 'THS']
 
 # Tickers to analyze by company in relevant industries
