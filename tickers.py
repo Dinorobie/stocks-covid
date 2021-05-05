abdbf1968a5075b0ccc49c90fe2ef479f932ee85
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-05-04 19:29:28 -0500

    Adds tickers to analyze

---
+++ tickers.py
@@ -0,0 +1,8 @@
+# Tickers to analyze by economic sector
+sector_symbol=["XLC","XLP","XLY","XLE","XLF","XLV", "XLI", "XLB", "XLRE", "XLK","XLU"]
+
+# Tickers to analze by industry inside relevant economic sectot
+energy_industry = ['EES', 'OGC']
+technology_industry = ['CEQ', 'EEI', 'ITS', 'SSE', 'SW', 'THS']
+
+# Tickers to analyze by company in relevant industries
