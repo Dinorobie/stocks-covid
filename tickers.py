b89a80c6d430ae32a02a4e95b264a943863e0b1e
Omar Gibran Rivera Galvan <omargib86@hotmail.com>
2021-05-06 19:44:19 -0500

    update

--- tickers.py
+++ tickers.py
@@ -3,6 +3,6 @@ sector_symbol=["XLC","XLP","XLY","XLE","XLF","XLV", "XLI", "XLB", "XLRE", "XLK",
 
 # Tickers to analze by industry inside relevant economic sectot
 energy_industry = ['EES', 'OGC']
-technology_industry = ['CEQ', 'EEI', 'ITS', 'SSE', 'SW', 'THS']
+technology_industry = ['AAPL','MSFT','V','MA','NVDA','PYPL','ADBE','INTC','CSCO','CRM']
 
 # Tickers to analyze by company in relevant industries
