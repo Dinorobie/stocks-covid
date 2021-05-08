79004f4d885fa8f0158537f3bf8174641233ddf3
viviach <viviana.aragonch@gmail.com>
2021-05-07 20:25:09 -0500

    updated technology tickers

--- tickers.py
+++ tickers.py
@@ -3,6 +3,6 @@ sector_symbol=["XLC","XLP","XLY","XLE","XLF","XLV", "XLI", "XLB", "XLRE", "XLK",
 
 # Tickers to analze by industry inside relevant economic sector
 energy_industry = ['XOM', 'CVX', 'COP', 'EOG', 'KMI', 'SLB', 'MPC', 'PSX', 'PXD', 'VLO']
-technology_industry = ['CEQ', 'EEI', 'ITS', 'SSE', 'SW', 'THS']
+technology_industry = ["AAPL", "MSFT", "V", "MA", "NVDA", "PYPL", "ADBE", "INTC", "CSCO", "CRM"]
 
 # Tickers to analyze by company in relevant industries
